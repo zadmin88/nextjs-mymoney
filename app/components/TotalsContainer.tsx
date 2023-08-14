85963f72dca315436c8526ee7a52196f1c482fe3
Simon Franco Angarita <zaimon88@gmail.com>
2023-08-13 20:31:37 -0500

    Correct the greeting Typo.

--- app/components/TotalsContainer.tsx
+++ app/components/TotalsContainer.tsx
@@ -14,7 +14,7 @@ const TotalsContainer: React.FC<any> = ({ moneyAccounts, currentUser }) => {
   return (
     <div className="bg-brand-lime  flex flex-col pt-10 px-6 pb-9 ">
       <h1 className="text-2xl font-semibold mb-10">
-        Hola, {currentUser.name.split(" ")[0]} :)
+        Hi, {currentUser.name.split(" ")[0]} :)
       </h1>
       <div className="grid grid-cols-2 gap-4">
         <TotalCardItem
